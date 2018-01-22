c94ce4ffa49191aa8208a3d9bd2b9a33e9bbac51
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-01-22 01:52:24 +0100

    start a pgn compression example

---
+++ examples/compression.rs
@@ -0,0 +1,194 @@
+extern crate pgn_reader;
+extern crate memmap;
+extern crate madvise;
+extern crate shakmaty;
+
+use pgn_reader::{Visitor, Skip, Reader, San};
+
+use shakmaty::{Chess, Role, Position, Setup, MoveList, Square, Move, Color, Piece};
+
+use memmap::Mmap;
+use madvise::{AccessPattern, AdviseMemory};
+
+use std::env;
+use std::fs::File;
+
+struct Histogram {
+    counts: [u64; 256],
+    pos: Chess,
+    skip: bool,
+}
+
+impl Histogram {
+    fn new() -> Histogram {
+        Histogram {
+            counts: [0; 256],
+            pos: Chess::default(),
+            skip: false,
+        }
+    }
+}
+
+impl<'pgn> Visitor<'pgn> for Histogram {
+    type Result = ();
+
+    fn begin_game(&mut self) {
+        self.pos = Chess::default();
+        self.skip = false;
+    }
+
+    fn header(&mut self, key: &'pgn [u8], _value: &'pgn [u8]) {
+        if key == b"FEN" {
+            self.skip = true;
+        }
+    }
+
+    fn end_headers(&mut self) -> Skip {
+        Skip(self.skip)
+    }
+
+    fn begin_variation(&mut self) -> Skip {
+        Skip(true)
+    }
+
+    fn san(&mut self, san: San) {
+        if !self.skip {
+            let mut legals = MoveList::new();
+            self.pos.legal_moves(&mut legals);
+
+            legals.sort_unstable_by_key(|m| {
+                let p = m.promotion().map(role_index).unwrap_or(0);
+                let c = m.capture().is_some();
+                let see = poor_mans_see(&self.pos, m);
+                let val = move_value(self.pos.turn(), m);
+                let from = m.from().expect("no drops");
+                //(p, c, see, val, m.to(), from)
+                (p, see, c, val, m.to(), from)
+            });
+
+            legals.reverse();
+
+            let idx = match legals.iter().position(|m| san.matches(m)) {
+                Some(idx) => idx,
+                None => {
+                    eprintln!("illegal san: {}", san);
+                    self.skip = true;
+                    return;
+                }
+            };
+
+            self.counts[idx] += 1;
+
+            self.pos.play_unchecked(&legals[idx]);
+        }
+    }
+
+    fn end_game(&mut self, _game: &'pgn [u8]) { }
+}
+
+fn role_index(role: Role) -> usize {
+    match role {
+        Role::Pawn => 0,
+        Role::Knight => 1,
+        Role::Bishop => 2,
+        Role::Rook => 3,
+        Role::Queen => 4,
+        Role::King => 5,
+    }
+}
+
+fn piece_value(piece: Piece, square: Square) -> i16 {
+    let sq = if piece.color.is_white() { square.flip_vertical() } else { square };
+    PSQT[role_index(piece.role)][usize::from(sq)]
+}
+
+fn move_value(turn: Color, m: &Move) -> i16 {
+    let role = m.role();
+    piece_value(role.of(turn), m.to()) - piece_value(role.of(turn), m.from().expect("no drops"))
+}
+
+fn poor_mans_see(pos: &Chess, m: &Move) -> i8 {
+    if (shakmaty::attacks::pawn_attacks(pos.turn(), m.to()) & pos.board().pawns() & pos.them()).any() {
+        -(role_index(m.role()) as i8)
+    } else {
+        0
+    }
+}
+
+static PSQT: [[i16; 64]; 6] = [
+    [
+         0,  0,  0,  0,  0,  0,  0,  0,
+        50, 50, 50, 50, 50, 50, 50, 50,
+        10, 10, 20, 30, 30, 20, 10, 10,
+         5,  5, 10, 25, 25, 10,  5,  5,
+         0,  0,  0, 20, 21,  0,  0,  0,
+         5, -5,-10,  0,  0,-10, -5,  5,
+         5, 10, 10,-31,-31, 10, 10,  5,
+         0,  0,  0,  0,  0,  0,  0,  0
+    ],
+    [
+        -50,-40,-30,-30,-30,-30,-40,-50,
+        -40,-20,  0,  0,  0,  0,-20,-40,
+        -30,  0, 10, 15, 15, 10,  0,-30,
+        -30,  5, 15, 20, 20, 15,  5,-30,
+        -30,  0, 15, 20, 20, 15,  0,-30,
+        -30,  5, 10, 15, 15, 11,  5,-30,
+        -40,-20,  0,  5,  5,  0,-20,-40,
+        -50,-40,-30,-30,-30,-30,-40,-50
+    ],
+    [
+        -20,-10,-10,-10,-10,-10,-10,-20,
+        -10,  0,  0,  0,  0,  0,  0,-10,
+        -10,  0,  5, 10, 10,  5,  0,-10,
+        -10,  5,  5, 10, 10,  5,  5,-10,
+        -10,  0, 10, 10, 10, 10,  0,-10,
+        -10, 10, 10, 10, 10, 10, 10,-10,
+        -10,  5,  0,  0,  0,  0,  5,-10,
+        -20,-10,-10,-10,-10,-10,-10,-20
+    ],
+    [
+          0,  0,  0,  0,  0,  0,  0,  0,
+          5, 10, 10, 10, 10, 10, 10,  5,
+         -5,  0,  0,  0,  0,  0,  0, -5,
+         -5,  0,  0,  0,  0,  0,  0, -5,
+         -5,  0,  0,  0,  0,  0,  0, -5,
+         -5,  0,  0,  0,  0,  0,  0, -5,
+         -5,  0,  0,  0,  0,  0,  0, -5,
+          0,  0,  0,  5,  5,  0,  0,  0
+    ],
+    [
+        -20,-10,-10, -5, -5,-10,-10,-20,
+        -10,  0,  0,  0,  0,  0,  0,-10,
+        -10,  0,  5,  5,  5,  5,  0,-10,
+         -5,  0,  5,  5,  5,  5,  0, -5,
+          0,  0,  5,  5,  5,  5,  0, -5,
+        -10,  5,  5,  5,  5,  5,  0,-10,
+        -10,  0,  5,  0,  0,  0,  0,-10,
+        -20,-10,-10, -5, -5,-10,-10,-20
+    ],
+    [
+        -30,-40,-40,-50,-50,-40,-40,-30,
+        -30,-40,-40,-50,-50,-40,-40,-30,
+        -30,-40,-40,-50,-50,-40,-40,-30,
+        -30,-40,-40,-50,-50,-40,-40,-30,
+        -20,-30,-30,-40,-40,-30,-30,-20,
+        -10,-20,-20,-20,-20,-20,-20,-10,
+         20, 20,  0,  0,  0,  0, 20, 20,
+          1, 30, 10,  0,  0, 10, 30,  2
+    ]
+];
+
+fn main() {
+    let mut histogram = Histogram::new();
+
+    for arg in env::args().skip(1) {
+        eprintln!("reading {} ...", arg);
+        let file = File::open(&arg).expect("fopen");
+        let pgn = unsafe { Mmap::map(&file).expect("mmap") };
+        pgn.advise_memory_access(AccessPattern::Sequential).expect("madvise");
+
+        Reader::new(&mut histogram, &pgn[..]).read_all();
+
+        println!("{:?}", &histogram.counts[..]);
+    }
+}
